5a18519fc84244712849ea481e73cd3b8fe51866
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-03-10 01:32:32 +0530

    setup file and create component

---
+++ react-crud-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Create from "./components/create";
+
+function App() {
+  return (
+    <div className="main">
+      <h2 className="main-header">React crud operations</h2>
+      <div>
+        <Create />
+      </div>
+    </div>
+  );
+}
+
+export default App;
